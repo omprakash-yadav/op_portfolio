ad89b5375dd76e2fcbd30dac127094c6140c5beb
omprakash yadav <omprakash9597@gmail.com>
2022-07-30 13:12:38 +0530

    op_portfolio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -114,6 +114,14 @@ function Home() {
     return window.addEventListener("scroll", () => {
       camera.rotation.z = window.scrollY * 0.001;
       camera.rotation.y = window.scrollY * 0.003;
+
+      const skillsBox = document.getElementById("homeskillsBox");
+
+      if (window.scrollY > 1500) {
+        skillsBox.style.animationName = "homeskillsBoxAnimationOn";
+      } else {
+        skillsBox.style.animationName = "homeskillsBoxAnimationOff";
+      }
     });
   }, []);
 
